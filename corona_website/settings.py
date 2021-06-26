19c596f5622301e32abf074bd1d28b1f77c207e0
HariRam1998 <HariRam1998@users.noreply.github.com>
2021-06-26 09:47:27 +0530

    edit

--- corona_website/settings.py
+++ corona_website/settings.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'corona_website.urls'
@@ -141,6 +142,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'allfiles/static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # HTTPS SETTINGS
 # SESSION_COOKIE_SECURE = True
