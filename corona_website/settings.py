8e78a33e07b7adce8ff0de0a0130685fd4042915
HariRam1998 <HariRam1998@users.noreply.github.com>
2021-06-28 02:02:25 +0530

    edit

--- corona_website/settings.py
+++ corona_website/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'corona_website.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-#'''
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
@@ -100,7 +100,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
+#'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
