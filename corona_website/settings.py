46c0dd91589ebe5be255baa6094564eba975a204
HariRam1998 <HariRam1998@users.noreply.github.com>
2021-06-28 13:43:01 +0530

    edit

--- corona_website/settings.py
+++ corona_website/settings.py
@@ -147,9 +147,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # HTTPS SETTINGS
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# SECURE_SSL_REDIRECT = True
 
 # Media Files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
